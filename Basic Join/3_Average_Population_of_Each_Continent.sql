52832c4bd1e8e69c613176942f5b357fa9904a0a
Onkar-Naik <on23337@gmail.com>
2024-12-17 00:43:20 +0530

    SOVED ALL BASIC JOIN SQL PROBLEMS OF HACKERRANK

---
+++ Basic Join/3_Average_Population_of_Each_Continent.sql
@@ -0,0 +1,8 @@
+-- Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) 
+-- and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+SELECT country.continent,FLOOR(AVG(city.population))
+FROM country
+INNER JOIN city
+ON country.code = city.countrycode
+GROUP BY 1;
